431cdbefb890377eae8a60652418fb0f87ff729d
Siwoww <154734554+Siwoww@users.noreply.github.com>
2025-06-12 16:04:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
-provider "aws" {
+/*provider "aws" {
     region = var.region
+}*/
+
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = ">= 5.99.1"
+        }
+    }
 }
 
 resource "aws_instance" "server" {
